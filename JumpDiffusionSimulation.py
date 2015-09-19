256c400ba86f7de9611b171886e478adb8e2ec14
Jesus Perez Colino <jpcolino@gmail.com>
2015-09-19 10:31:08 +0200

    minor changes

--- JumpDiffusionSimulation.py
+++ JumpDiffusionSimulation.py
@@ -1,6 +1,8 @@
 import numpy as np
 from abc import ABCMeta, abstractmethod
 
+__author__ = 'Jesus Perez Colino'
+
 
 class EuropeanOption(object):
     """ Abstract Class for European options. Partially implemented.
@@ -124,80 +126,5 @@ class JumpDiffusionFFT(EuropeanOption):
             return value
 
     @property
-    def value(self):
-    ''' Valuation of European call option in M76 model via
-    Carr-Madan (1999) Fourier-based approach.
-
-    Parameters
-    ==========
-    S0: float
-        initial stock/index level
-    K: float
-        strike price
-    T: float
-        time-to-maturity (for t=0)
-    r: float
-        constant risk-free short rate
-    sigma: float
-        volatility factor in diffusion term
-    lamb: float
-        jump intensity
-    mu: float
-        expected jump size
-    delta: float
-        standard deviation of jump
-
-    Returns
-    =======
-    call_value: float
-        European call option present value
-    '''
-    k = np.log(K / S0)
-    x0 = math.log(S0 / S0)
-    g = 2  # factor to increase accuracy
-    N = g * 4096
-    eps = (g * 150.) ** -1
-    eta = 2 * math.pi / (N * eps)
-    b = 0.5 * N * eps - k
-    u = np.arange(1, N + 1, 1)
-    vo = eta * (u - 1)
-    # Modificatons to Ensure Integrability
-    if S0 >= 0.95 * K:  # ITM case
-        alpha = 1.5
-        v = vo - (alpha + 1) * 1j
-        mod_char_fun = math.exp(-r * T) * characteristic_function(
-                                    v, x0, T, r, sigma, lamb, mu, delta) \
-                / (alpha ** 2 + alpha - vo ** 2 + 1j * (2 * alpha + 1) * vo)
-    else:  # OTM case
-        alpha = 1.1
-        v = (vo - 1j * alpha) - 1j
-        mod_char_fun_1 = math.exp(-r * T) * (1 / (1 + 1j * (vo - 1j * alpha))
-                                   - math.exp(r * T) / (1j * (vo - 1j * alpha))
-                                   - M76_characteristic_function(
-                                     v, x0, T, r, sigma, lamb, mu, delta)
-                / ((vo - 1j * alpha) ** 2 - 1j * (vo - 1j * alpha)))
-        v = (vo + 1j * alpha) - 1j
-        mod_char_fun_2 = math.exp(-r * T) * (1 / (1 + 1j * (vo + 1j * alpha))
-                                   - math.exp(r * T) / (1j * (vo + 1j * alpha))
-                                   - M76_characteristic_function(
-                                     v, x0, T, r, sigma, lamb, mu, delta)
-                / ((vo + 1j * alpha) ** 2 - 1j * (vo + 1j * alpha)))
-
-    # Numerical FFT Routine
-    delt = np.zeros(N, dtype=np.float)
-    delt[0] = 1
-    j = np.arange(1, N + 1, 1)
-    SimpsonW = (3 + (-1) ** j - delt) / 3
-    if S0 >= 0.95 * K:
-        fft_func = np.exp(1j * b * vo) * mod_char_fun * eta * SimpsonW
-        payoff = (fft(fft_func)).real
-        call_value_m = np.exp(-alpha * k) / math.pi * payoff
-    else:
-        fft_func = (np.exp(1j * b * vo)
-                    * (mod_char_fun_1 - mod_char_fun_2)
-                    * 0.5 * eta * SimpsonW)
-        payoff = (fft(fft_func)).real
-        call_value_m = payoff / (np.sinh(alpha * k) * math.pi)
-    pos = int((k + b) / eps)
-    call_value = call_value_m[pos]
-    return call_value * S0
+    def value(self):  # TODO
+        pass
