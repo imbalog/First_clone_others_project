7a81164ade9b57b2e7aad813149d9e4455f31e5e
Jesus Perez Colino <jpcolino@gmail.com>
2015-10-13 14:36:45 +0200

    minor changes

--- MonteCarloParallel.py
+++ MonteCarloParallel.py
@@ -1,40 +1,40 @@
 class EuropeanCallOption:
-	def __init__(self, sigma, S, K, T, r):
-		self.sigma = sigma
-		self.S = float(S)
-		self.K = float(K)
-		self.T = float(T)
-		self.r = float(r)
+    def __init__(self, sigma, S, K, T, r):
+        self.sigma = float(sigma)
+        self.S = float(S)
+        self.K = float(K)
+        self.T = float(T)
+        self.r = float(r)
 
-	def priceBS(self):
-		import numpy as np
-		from scipy.stats import norm
-		d1 = 1.0 / float(self.sigma * np.sqrt(self.T))
-		d1 *= np.log(self.K / self.S) + (self.r + 0.5 * self.sigma ** 2) * self.T
-		d2 = d1 - self.sigma * np.sqrt(self.T)
-		return norm.cdf(d1) * self.S - norm.cdf(d2) * self.K * np.exp(-1.0 * self.r * self.T)
+    def priceBS(self):
+        import numpy as np
+        from scipy.stats import norm
+        d1 = 1.0 / float(self.sigma * np.sqrt(self.T))
+        d1 *= np.log(self.K / self.S) + (self.r + 0.5 * self.sigma ** 2) * self.T
+        d2 = d1 - self.sigma * np.sqrt(self.T)
+        return norm.cdf(d1) * self.S - norm.cdf(d2) * self.K * np.exp(-1.0 * self.r * self.T)
 
-	def priceMC(self, simulations=1, seed=1234567890, antithetic=True):
-		import numpy as np
-		from numpy.random import RandomState
-		if seed is not None:
-			assert type(seed) is int
-			rnd = RandomState(seed)
-		else:
-			rnd = RandomState()
-		sum_payoff = 0
-		for i in range(simulations):
-			brownian = rnd.normal()
-			sum_payoff += np.maximum(self.S *
-			                         np.exp((self.r - 0.5 * self.sigma**2) * self.T +
-			                                self.sigma * np.sqrt(self.T) * brownian) -
-			                         self.K, 0)
-		return np.exp(-1.0 * self.r * self.T) * sum_payoff / float(simulations)
+    def priceMC(self, simulations=1, seed=1234567890, antithetic=True):
+        import numpy as np
+        if seed is not None:
+            assert type(seed) is int
+            np.random.seed(seed)
+        if antithetic:
+            brownian = np.random.standard_normal(int(np.ceil(simulations/2.)))
+            brownian = np.concatenate((brownian, -brownian))
+        else:
+            brownian = rnd.normal(simulations)
+
+        price_terminal = self.S * np.exp((self.r - 0.5 * self.sigma ** 2) * self.T +
+                                          self.sigma * np.sqrt(self.T) * brownian)
+        payoff = np.maximum((price_terminal - self.K), 0)
+        return np.exp(-1.0 * self.r * self.T) * np.sum(payoff)/ float(simulations)
 
 
 def eval_price_in_pool(simulations):
-	call = EuropeanCallOption(0.2, 100, 100, 1, 0.05)
-	return call.priceMC(simulations, seed=os.getpid())
+    import os
+    call = EuropeanCallOption(0.2, 100., 100., 1, 0.05)
+    return call.priceMC(simulations, seed=os.getpid())
 
 
 if __name__ == '__main__':
